24ded4d3f66a49837973e8f4b8342791355d99d3
Maiko Tan <maiko.tan.coding@gmail.com>
2023-12-14 13:24:12 +0800

    fix: add work-vm to root level configuration

--- flake.nix
+++ flake.nix
@@ -10,18 +10,34 @@
       machines = ./machines/default.nix;
     in
     {
-      nixosConfigurations.nixos-vm = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./config.nix
-          ./machines/nixos-vm/config.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.maiko = import ./home.nix;
-          }
-        ];
+      nixosConfigurations = {
+        nixos-vm = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./config.nix
+            ./machines/nixos-vm/config.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.maiko = import ./home.nix;
+            }
+          ];
+        };
+
+        work-vm = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./config.nix
+            ./machines/work-vm/config.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.maiko = import ./home.nix;
+            }
+          ];
+        };
       };
     };
 }
