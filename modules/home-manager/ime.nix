0864274235e03d1ceab6b2b160d07a8394b543ff
Maiko Tan <maiko.tan.coding@gmail.com>
2024-04-07 08:48:28 +0800

    fix: remove settings for fctix5 profile

--- modules/home-manager/ime.nix
+++ modules/home-manager/ime.nix
@@ -12,38 +12,5 @@ patch:
       '';
       target = "./.local/share/fcitx5/rime/default.custom.yaml";
     };
-    "fcitx5/profile" = {
-      text = ''
-[Groups/0]
-# Group Name
-Name=Default
-# Layout
-Default Layout=us
-# Default Input Method
-DefaultIM=rime
-
-[Groups/0/Items/0]
-# Name
-Name=keyboard-us
-
-[Groups/0/Items/1]
-# Name
-Name=keyboard-fr
-
-[Groups/0/Items/2]
-# Name
-Name=keyboard-de
-
-[Groups/0/Items/3]
-# Name
-Name=rime
-# Layout
-Layout=us
-
-[GroupOrder]
-0=Default
-      '';
-      target = "./.config/fcitx5/profile";
-    };
   };
 }
