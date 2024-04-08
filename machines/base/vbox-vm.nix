6da37d2ac3ff9cc671482fa57e3c8a7ec567542b
Maiko Tan <maiko.tan.coding@gmail.com>
2024-04-08 15:28:11 +0800

    refactor: remove unused vbox services

--- machines/base/vbox-vm.nix
+++ machines/base/vbox-vm.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, config, ... }:
+{ ... }:
 
 {
   # Use the systemd-boot EFI boot loader.
@@ -12,25 +12,6 @@
   console = {
     font = "Lat2-Terminus16";
     useXkbConfig = true; # use xkb.options in tty.
-  };
-
-  # Enhance virtualbox VM experience.
-  services.xserver.videoDrivers = lib.mkForce [ "vmware" "virtualbox" "modesetting" ];
-  systemd.user.services = let
-    vbox-client = desc: flags: {
-      description = "VirtualBox Guest: ${desc}";
-
-      wantedBy = [ "graphical-session.target" ];
-      requires = [ "dev-vboxguest.device" ];
-      after = [ "dev-vboxguest.device" ];
-
-      unitConfig.ConditionVirtualization = "oracle";
-
-      serviceConfig.ExecStart = "${config.boot.kernelPackages.virtualboxGuestAdditions}/bin/VBoxClient -fv ${flags}";
-      };
-  in {
-    virtualbox-resize = vbox-client "Resize" "--vmsvga";
-    virtualbox-clipboard = vbox-client "Clipboard" "--clipboard";
   };
 
   virtualisation.virtualbox.guest = {
--- modules/home-manager/ime.nix
+++ modules/home-manager/ime.nix
@@ -4,11 +4,11 @@
   home.file = {
     "fcitx5/rime/default.custom.yaml" = {
       text = ''
-patch:
-  schema_list:
-    - schema: double_pinyin
-    - schema: luna_pinyin
-    - schema: japanese
+        patch:
+          schema_list:
+            - schema: double_pinyin
+            - schema: luna_pinyin
+            - schema: japanese
       '';
       target = "./.local/share/fcitx5/rime/default.custom.yaml";
     };
