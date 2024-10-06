c397cdc19b498cedb6bfd7c78438900fa1ee13ef
Maiko Tan <maiko.tan.coding@gmail.com>
2024-10-06 14:21:47 +0800

    fix: remove vscode extensions, instead managed by itself

--- modules/home-manager/vscode/default.nix
+++ modules/home-manager/vscode/default.nix
@@ -7,23 +7,5 @@
     package = (code-insiders.packages."x86_64-linux".vscode-insider.overrideAttrs (oldAttrs: {
       buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 ];
     }));
-
-    extensions = with pkgs.vscode-extensions; [
-      WakaTime.vscode-wakatime
-      rust-lang.rust-analyzer
-      jnoortheen.nix-ide
-      eamodio.gitlens
-      github.copilot
-      bbenoist.nix
-      mkhl.direnv
-      golang.go
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "volar";
-        publisher = "Vue";
-        version = "2.0.8";
-        sha256 = "3RQ8dBy83QblARNMjy5RliVYSxEUdJA/3yyWcLK0wVQ=";
-      }
-    ];
   };
 }
