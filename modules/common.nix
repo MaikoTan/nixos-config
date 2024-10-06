d3b6ddf606d3f98eeca7dd5eac0d1d0812be90c5
Maiko Tan <maiko.tan.coding@gmail.com>
2024-10-06 14:35:53 +0800

    fix: sound not working in 24.05

--- modules/common.nix
+++ modules/common.nix
@@ -35,6 +35,8 @@
   services.printing.enable = true;
 
   # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
 }
