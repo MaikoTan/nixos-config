5e43009fda5ecc6f0d684ee829ea97317535ee76
Maiko Tan <maiko.tan.coding@gmail.com>
2024-10-07 15:40:54 +0800

    feat: add desktop configuration with GRUB boot loader and NetworkManager support

---
+++ machines/base/desktop.nix
@@ -0,0 +1,20 @@
+{ ... }:
+
+{
+  imports = [ ./config.nix ];
+
+  # Desktop environment use GRUB as the boot loader.
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    device = "nodev";
+  };
+
+  services.networkmanager.enable = true;
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    useXkbConfig = true; # use xkb.options in tty.
+  };
+}
