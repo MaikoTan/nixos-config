3d98c235bd1375e5991ffe42400fce547d8e584a
Maiko Tan <maiko.tan.coding@gmail.com>
2024-10-07 10:39:48 +0800

    feat: add time zone configuration and improve imports handling

--- modules/default.nix
+++ modules/default.nix
@@ -1,15 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./common.nix
-    ./docker.nix
-    ./ime.nix
-    ./nix.nix
-  ];
-
-  # Set your time zone.
-  time.timeZone = "Asia/Shanghai";
+  # This code snippet defines an `imports` attribute that filters and includes 
+  # all `.nix` files from the current directory. It uses `builtins.readDir` to 
+  # read the directory contents, `builtins.attrValues` to get the file paths, 
+  # and `builtins.filter` along with `builtins.match` to select only the files 
+  # with a `.nix` extension.
+  imports = builtins.filter (path: builtins.match ".*\\.nix$" path != null) (builtins.attrValues (builtins.readDir (toString ./.)));
 
   programs.gnupg.agent = {
     enable = true;
--- modules/nix.nix
+++ modules/nix.nix
@@ -4,17 +4,30 @@
   nixpkgs.config.allowUnfree = true;
 
   nix.settings = {
+    /*
+     * Configuration for trusted users in Nix.
+     * 
+     * This section specifies the users who are allowed to perform privileged Nix operations.
+     * - "root": The root user has full administrative privileges.
+     * - "@wheel": Members of the wheel group are also granted administrative privileges.
+     */
     trusted-users = [
       "root"
       "@wheel"
     ];
 
-    substituters = [
+    /*
+     * This configuration sets the `substituters` option for Nix package manager.
+     * If the system's time zone is set to "Asia/Shanghai", it uses a list of
+     * substituters from various Chinese university mirrors and the official NixOS cache.
+     * Otherwise, it falls back to the default substituters specified in `config.nix.settings.substituters`.
+     */
+    substituters = if config.time.timeZone == "Asia/Shanghai" then [
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://cache.nixos.org"
-    ];
+    ] else config.nix.settings.substituters or [];
   };
 
   nix.extraOptions = ''
