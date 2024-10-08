78058cee0798c6d11fda70020864b8b0e4c46f37
Maiko Tan <maiko.tan.coding@gmail.com>
2024-10-08 13:46:51 +0800

    feat: add vscode snippets

---
+++ modules/home-manager/vscode/user.nix
@@ -0,0 +1,11 @@
+# This file is used to configure the user environment for the VSCode extension.
+
+{ config, ... }:
+
+{
+  # symlink `./snippets` to `~/.config/Code/User/snippets` or `~/.config/Code - Insiders/User/snippets` if using the Insiders edition
+  home.file."vscode-snippets" = {
+    source = ./snippets;
+    target = "${config.home.homeDirectory}/snippets";
+  };
+}
