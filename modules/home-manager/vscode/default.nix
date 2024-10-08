7fc8687ec567e74ba1b4aafec890a2e1c92a7bc5
Maiko Tan <maiko.tan.coding@gmail.com>
2024-10-08 13:53:59 +0800

    fix: isInsiders flag

--- modules/home-manager/vscode/default.nix
+++ modules/home-manager/vscode/default.nix
@@ -3,6 +3,7 @@
 {
   programs.vscode = {
     enable = true;
+    isInsiders = true;
     enableExtensionUpdateCheck = false;
     package = (code-insiders.packages."x86_64-linux".vscode-insider.overrideAttrs (oldAttrs: {
       buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 ];
--- modules/home-manager/vscode/user.nix
+++ modules/home-manager/vscode/user.nix
@@ -6,6 +6,8 @@
   # symlink `./snippets` to `~/.config/Code/User/snippets` or `~/.config/Code - Insiders/User/snippets` if using the Insiders edition
   home.file."vscode-snippets" = {
     source = ./snippets;
-    target = "${config.home.homeDirectory}/snippets";
+    target = "${config.home.homeDirectory}/.config/" +
+      (if config.programs.vscode.isInsiders then "Code - Insiders" else "Code") +
+      "/User/snippets";
   };
 }
