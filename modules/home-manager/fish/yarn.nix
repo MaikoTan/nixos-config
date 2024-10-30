6f14a0b1a9539978be061bad692aef33fadc841e
Maiko Tan <maiko.tan.coding@gmail.com>
2024-10-30 09:31:58 +0800

    feat: add jq and findutils to ensure yarn workspace completions function correctly

--- modules/home-manager/fish/yarn.nix
+++ modules/home-manager/fish/yarn.nix
@@ -1,10 +1,19 @@
 {}:
 
 {
+  # Ensure jq and xargs are installed
+  environment.systemPackages = with pkgs; [
+    jq
+    findutils # Provides `xargs`
+    # grep and sort should already provided by coreutils that is installed by default
+  ];
+
   xdg.configFile.fishCompletionYarn = {
     executable = true;
     target = "fish/completions/yarn.fish";
     text = ''
+      # Completions for `yarn workspace <TAB>` command
+      # It lists all workspaces in the current directory based on the package.json file
       function __fish_yarn_get_workspaces
         if type -q jq && test -f package.json
           command \
