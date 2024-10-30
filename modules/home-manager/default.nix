831a448c51bbe76f87fc27a3c3852d771a51e47c
Maiko Tan <maiko.tan.coding@gmail.com>
2024-10-30 13:15:55 +0800

    feat: enable dconf program in home-manager configuration

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -35,6 +35,8 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  programs.dconf.enable = true;
+
   programs.direnv = {
     enable = true;
     enableFishIntegration = true;
