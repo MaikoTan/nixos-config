31d995ce4c90c93cf6a4ebec2e280df5091fc6e7
Maiko Tan <maiko.tan.coding@gmail.com>
2025-06-26 11:00:35 +0800

    refactor: remove user.nix and simplify vscode configuration

--- modules/home-manager/vscode/default.nix
+++ modules/home-manager/vscode/default.nix
@@ -1,16 +1,11 @@
 { pkgs, code-insiders, ... }:
 
 {
-  imports = [
-    ./user.nix
-  ];
   programs.vscode = {
     enable = true;
     # isInsiders = true; # No need to set this option since the flake input already set it in their package definition.
-    profiles.default = {
-      enableUpdateCheck = false;
-      enableExtensionUpdateCheck = false;
-    };
+
+    mutableExtensionsDir = true; # Allow VS Code to manage extensions as well as user settings.
     package = (code-insiders.packages."x86_64-linux".vscode-insider.overrideAttrs (oldAttrs: {
       buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 ];
     }));
