0d9b340eb5c86805d9d67dcd18cfc7c3272e6120
Maiko Tan <maiko.tan.coding@gmail.com>
2025-06-26 09:54:48 +0800

    fix: move enableExtensionUpdateCheck to profiles.default in vscode configuration

--- modules/home-manager/vscode/default.nix
+++ modules/home-manager/vscode/default.nix
@@ -4,7 +4,10 @@
   programs.vscode = {
     enable = true;
     # isInsiders = true; # No need to set this option since the flake input already set it in their package definition.
-    enableExtensionUpdateCheck = false;
+    profiles.default = {
+      enableUpdateCheck = false;
+      enableExtensionUpdateCheck = false;
+    };
     package = (code-insiders.packages."x86_64-linux".vscode-insider.overrideAttrs (oldAttrs: {
       buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 ];
     }));
