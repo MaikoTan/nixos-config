ff6c63bfe7f3eacbe73d3d5f02486850e0c64579
Maiko Tan <maiko.tan.coding@gmail.com>
2025-09-19 17:37:15 +0800

    feat: refactor vscode configuration and move to separate insiders.nix file

--- modules/home-manager/vscode/default.nix
+++ modules/home-manager/vscode/default.nix
@@ -1,28 +1,7 @@
-{ lib, pkgs, code-insiders, config, ... }:
-
-let
-
-  package = (code-insiders.packages."x86_64-linux".vscode-insider.overrideAttrs (oldAttrs: {
-    buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 ];
-  }));
-  editor = "${package}/bin/code-insiders --wait";
-
-in
+{ ... }:
 
 {
-  programs.vscode = {
-    enable = true;
-    # isInsiders = true; # No need to set this option since the flake input already set it in their package definition.
-
-    mutableExtensionsDir = true; # Allow VS Code to manage extensions as well as user settings.
-    inherit package;
-  };
-
-  programs.git.settings = {
-    editor = lib.mkIf config.programs.git.enable editor;
-  };
-
-  programs.gh.settings = {
-    editor = lib.mkIf config.programs.gh.enable editor;
-  };
+  imports = [
+    ./insiders.nix
+  ];
 }
---
+++ modules/home-manager/vscode/insiders.nix
@@ -0,0 +1,30 @@
+{ lib, pkgs, code-insiders, config, ... }:
+
+let
+
+  package = (code-insiders.packages."x86_64-linux".vscode-insider.overrideAttrs (oldAttrs: {
+    buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 ];
+  }));
+  editor = "${package}/bin/code-insiders --wait";
+
+in
+
+{
+  programs.vscode = {
+    enable = true;
+    # isInsiders = true; # No need to set this option since the flake input already set it in their package definition.
+
+    mutableExtensionsDir = true; # Allow VS Code to manage extensions as well as user settings.
+    inherit package;
+  };
+
+  programs.git.extraConfig = {
+    core = {
+      inherit editor;
+    };
+  };
+
+  programs.gh.settings = {
+    inherit editor;
+  };
+}
