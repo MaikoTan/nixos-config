65ac207077af7a34e2dadd8da03ab714c6935cd3
Maiko Tan <maiko.tan.coding@gmail.com>
2025-09-19 15:56:49 +0800

    feat: add initial configuration files for company setup

---
+++ machines/company/config.nix
@@ -0,0 +1,37 @@
+{ nixos-hardware, ... }:
+
+{
+  imports =
+    [
+      # Import common configuration from nixos
+      nixos-hardware.nixosModules.common-cpu-intel
+      ../base/desktop.nix
+      ./hardware.nix
+    ];
+
+  # Set your time zone.
+  time.timeZone = "Asia/Shanghai";
+
+  networking.hostName = "company";
+
+  networking = {
+    interfaces.enp2s0 = {
+      ipv4.addresses = [{
+        address = "192.168.30.80";
+        prefixLength = 24;
+      }];
+    };
+    defaultGateway = {
+      address = "192.168.30.254";
+      interface = "enp2s0";
+    };
+  };
+
+  # Automatically join the ZeroTier network.
+  services.zerotierone = {
+    enable = true;
+    joinNetworks = [
+      "35c192ce9bf3258b"
+    ];
+  };
+}
