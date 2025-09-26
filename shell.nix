2286117c85cc73e224e5f26f15bb90a3eea05d60
Maiko Tan <maiko.tan.coding@gmail.com>
2025-09-26 13:39:44 +0800

    refactor: enhance shell.nix with cachix integration and editor configuration

--- shell.nix
+++ shell.nix
@@ -5,5 +5,12 @@ pkgs.mkShell {
   packages = with pkgs; [
     nixVersions.latest
     nixos-rebuild
+    cachix
   ];
+  shellHook = ''
+    export EDITOR=vim
+    export VISUAL=vim
+
+    cachix use maiko-nixos-build || true
+  '';
 }
