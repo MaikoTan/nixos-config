6b1c0cd186a2dfcdb17763488e9fb1ed2ff74df7
Maiko Tan <maiko.tan.coding@gmail.com>
2025-09-27 13:59:10 +0000

    fix: add trusted public keys for nix

--- modules/nix.nix
+++ modules/nix.nix
@@ -29,6 +29,12 @@
         "https://cache.nixos.org"
       ]
     ];
+
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "maiko-nixos-build.cachix.org-1:4EC5TVcsNL76xt7aqVoBpK0PoXPXuOF/kTLyjFyb8SM="
+    ];
   };
 
   config.nix.extraOptions = ''
