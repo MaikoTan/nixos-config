70cbccc377f24a4b12d876bd42f26ade04f31b5d
Maiko Tan <maiko.tan.coding@gmail.com>
2025-10-24 13:41:28 +0800

    fix: adjust docker module — use daemon.settings, drop rootless.enable, and add lib param for users.extraGroups

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,11 +1,17 @@
-{ ... }:
+{ lib, ... }:
 
 {
   virtualisation.docker = {
     enable = true;
-    rootless.enable = true;
-    rootless.daemon.settings = {
+    daemon.settings = {
       ipv6 = true;
+      registry-mirrors = [
+        # China Mainland Mirrors
+        "https://docker.mirrors.ustc.edu.cn"
+        "https://mirror.ccs.tencentyun.com"
+      ];
     };
   };
+
+  users.users.maiko.extraGroups = lib.mkAfter [ "docker" ];
 }
