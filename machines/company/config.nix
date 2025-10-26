4608363be4ccd859db0d299829d6893520e24b86
Maiko Tan <maiko.tan.coding@gmail.com>
2025-10-26 11:46:56 +0800

    feat: add gnome-remote-desktop support

--- machines/company/config.nix
+++ machines/company/config.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   nixos-hardware,
   ...
 }:
@@ -37,6 +38,7 @@
       interface = "enp2s0";
     };
     nameservers = ["192.168.30.254"];
+    firewall.allowedTCPPorts = [ 3389 ];
   };
 
   nix.use-china-mirrors = true;
@@ -70,4 +72,20 @@
   services.tailscale = {
     enable = true;
   };
+
+  # https://nixos.wiki/wiki/Remote_Desktop
+  # Enable the GNOME RDP components
+  services.gnome.gnome-remote-desktop.enable = true;
+
+  # Ensure the service starts automatically at boot so the settings panel appears
+  systemd.services.gnome-remote-desktop = {
+    wantedBy = [ "graphical.target" ];
+  };
+
+  # Open the default RDP port (3389)
+  # networking.firewall.allowedTCPPorts = [ 3389 ];
+
+  # Disable autologin to avoid session conflicts
+  services.displayManager.autoLogin.enable = false;
+  services.getty.autologinUser = null;
 }
