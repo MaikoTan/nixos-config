de5dd0f7bd15d71e48b8e298f2a411be7851707d
Maiko Tan <maiko.tan.coding@gmail.com>
2025-11-25 15:58:51 +0800

    refactor(flake): add default development shell configuration

--- flake.nix
+++ flake.nix
@@ -109,5 +109,15 @@
     in
     {
       nixosConfigurations = (builtins.foldl' (a: b: a // b) { } machineConfigs);
+
+      devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+        buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
+          nixVersions.latest
+          nixos-rebuild
+          fish
+          nixfmt
+          cachix
+        ];
+      };
     };
 }
