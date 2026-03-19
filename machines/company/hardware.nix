13a6c4172361718d77db3bace6712e2e89b17306
Maiko Tan <maiko.tan.coding@gmail.com>
2026-03-19 11:38:10 +0800

    fix: optimize zram swap settings and adjust kernel sysctl parameters

--- machines/company/hardware.nix
+++ machines/company/hardware.nix
@@ -118,15 +118,19 @@ in
   # Enable zram swap for better performance on systems with low RAM
   zramSwap = {
     enable = true;
-    memoryPercent = 150;
+    memoryPercent = 300;
     algorithm = "zstd";
     priority = 100;
   };
 
   boot.kernel.sysctl = {
     # Increase swappiness to use swap more aggressively
-    "vm.swappiness" = 120;
-    "vm.vfs_cache_pressure" = 100;
+    # See: https://wiki.archlinux.org/title/Zram#Optimizing_swap_on_zram
+    "vm.swappiness" = 180;
+    "vm.vfs_cache_pressure" = 50;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+    "vm.page-cluster" = 0;
     # Enable BBR congestion control
     "net.core.default_qdisc" = "fq";
     "net.ipv4.tcp_congestion_control" = "bbr";
