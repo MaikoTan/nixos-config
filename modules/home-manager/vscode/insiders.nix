77ab471af3e37a02996b32480f5f3f763eeadfc6
Maiko Tan <maiko.tan.coding@gmail.com>
2026-05-26 11:03:05 +0800

    feat: enhance vscode-insider package with post-patch for `@vscode/ripgrep-universal`

--- modules/home-manager/vscode/insiders.nix
+++ modules/home-manager/vscode/insiders.nix
@@ -1,5 +1,6 @@
 {
   inputs,
+  lib,
   pkgs,
   ...
 }:
@@ -7,12 +8,64 @@
 let
 
   package = inputs.code-insiders.packages."x86_64-linux".vscode-insider.overrideAttrs (oldAttrs: {
-    buildInputs = oldAttrs.buildInputs ++ [
-      pkgs.webkitgtk_4_1
-      pkgs.libsoup_3
-      pkgs.openssl
-      pkgs.curl
-    ];
+    buildInputs =
+      oldAttrs.buildInputs
+      ++ (with pkgs; [
+        webkitgtk_4_1
+        libsoup_3
+        openssl
+        curl
+        musl
+      ]);
+    # Copied and adapted from https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/editors/vscode/generic.nix#L382
+    postPatch =
+      lib.optionalString pkgs.stdenv.hostPlatform.isLinux (
+        # disable update checks
+        ''
+          tmpProductJson="$(mktemp)"
+          jq 'del(.updateUrl, .backupUpdateUrl)' resources/app/product.json > "$tmpProductJson"
+          mv "$tmpProductJson" resources/app/product.json
+        ''
+        # this is a fix for "save as root" functionality
+        + ''
+          packed="resources/app/node_modules.asar"
+          unpacked="resources/app/node_modules"
+          asar extract "$packed" "$unpacked"
+          substituteInPlace $unpacked/@vscode/sudo-prompt/index.js \
+            --replace-fail "/usr/bin/pkexec" "/run/wrappers/bin/pkexec" \
+            --replace-fail "/bin/bash" "${pkgs.bash}/bin/bash"
+          rm -rf "$packed"
+        ''
+        # without this symlink loading JsChardet, the library that is used for auto encoding detection when files.autoGuessEncoding is true,
+        # fails to load with: electron/js2c/renderer_init: Error: Cannot find module 'jschardet'
+        # and the window immediately closes which renders VSCode unusable
+        # see https://github.com/NixOS/nixpkgs/issues/152939 for full log
+        + ''
+          ln -rs "$unpacked" "$packed"
+        ''
+      )
+      + (
+        let
+          ripgrepPath = "@vscode/ripgrep-universal/bin/${lib.toLower pkgs.stdenv.hostPlatform.uname.system}-${pkgs.stdenv.hostPlatform.efiArch}/rg";
+          vscodeRipgrep =
+            if pkgs.stdenv.hostPlatform.isDarwin then
+              if lib.versionAtLeast oldAttrs.version "1.94.0" then
+                "Contents/Resources/app/node_modules/${ripgrepPath}"
+              else
+                "Contents/Resources/app/node_modules.asar.unpacked/${ripgrepPath}"
+            else
+              "resources/app/node_modules/${ripgrepPath}";
+        in
+        if !false then
+          ''
+            rm ${vscodeRipgrep}
+            ln -s ${pkgs.ripgrep}/bin/rg ${vscodeRipgrep}
+          ''
+        else
+          ''
+            chmod +x ${vscodeRipgrep}
+          ''
+      );
   });
   editor = "${package}/bin/code-insiders --wait";
 
